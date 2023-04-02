75d265e6b7f9f49816c30070a21313ea7dd5a3df
Виктория Кирина <viktoriakirina@MacBook-Pro-Viktoria.local>
2023-04-02 03:41:27 +0300

    edit js file

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const burgerBtn = document.querySelector('.burger_btn')
 const headerBlock = document.querySelector('.header__block')
 const header = document.querySelector('.header')
 const hero = document.querySelector('.hero')
+const formats = document.querySelector('.formats__block')
 
 // if (document.documentElement.clientWidth < 480) {
 //   headerBlock.classList.add('hide')
@@ -11,4 +12,5 @@ burgerBtn.addEventListener('click', () => {
   headerBlock.classList.toggle('open');
   header.classList.toggle('open');
   hero.classList.toggle('hide');
+  formats.classList.toggle('hide');
 })
