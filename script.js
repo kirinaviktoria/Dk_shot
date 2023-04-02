85a1c9fb2dc2d6f56b65a24327d97f03b83f62b2
Виктория Кирина <viktoriakirina@MacBook-Pro-Viktoria.local>
2023-04-02 03:24:34 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+const burgerBtn = document.querySelector('.burger_btn')
+const headerBlock = document.querySelector('.header__block')
+const header = document.querySelector('.header')
+const hero = document.querySelector('.hero')
+
+// if (document.documentElement.clientWidth < 480) {
+//   headerBlock.classList.add('hide')
+// };
+
+burgerBtn.addEventListener('click', () => {
+  headerBlock.classList.toggle('open');
+  header.classList.toggle('open');
+  hero.classList.toggle('hide');
+})
