4c65fa35838fd9ad47928e1d4ee5566875fbeabd
Vicktoria Kirina <kirinavi@outlook.com>
2023-04-12 18:28:56 +0300

    added models block

--- script.js
+++ script.js
@@ -20,5 +20,28 @@ const hideAll = () => {
   containers.forEach((item) => {
     item.classList.toggle('hide');
   })
-  
 }
+
+const swiper = new Swiper('.mySwiper', {
+  // Optional parameters
+  // direction: 'horizontal',
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  // scrollbar: {
+  //   el: '.swiper-scrollbar',
+  // },
+
+  mousewhell: true,
+});
