d2c6e147496a4eb36ff36b4ee452d28db6a883b5
Vicktoria Kirina <kirinavi@outlook.com>
2023-05-15 22:19:33 +0300

    new adaptive

--- script.js
+++ script.js
@@ -127,3 +127,23 @@ const swiper = new Swiper('.mySwiper', {
 
   mousewhell: true,
 });
+
+const models_swiper = new Swiper('.modelsSwiper', {
+  // Optional parameters
+  // direction: 'horizontal',
+  effect: 'slide',
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  mousewhell: true,
+});
